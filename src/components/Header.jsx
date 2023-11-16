fe3d440c4f63eae4a91d1d2d3c6002f855cdfdb5
nikhilputhumana <nikhilp.csb1923@saintgits.org>
2023-11-16 19:08:40 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import '../style.css'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                // src="../assets/troll-face.png" 
+                src="/troll-face.png"
+                alt='image not loaded'
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
